6c17ba664bc09a02dcb8429e753e77d5b112253d
linwenbo <linwenbo1994@gmail.com>
2020-01-06 01:11:11 +0800

    20200106

--- init.el
+++ init.el
@@ -2,7 +2,45 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+
+(when ( >= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (setq package-archives
+	'(("gnu" . "http://elpa.emacs-china.org/gnu/")
+	  ("melpa" . "http://elpa.emacs-china.org/melpa/")
+	  ("org" . "http://elpa.emacs-china.org/org/"))))
+
+(require 'cl)
+
+;; Add Packages
+(defvar linwenbo/packages '(
+			    ;; auto completion
+			    company
+			    ;; editor
+			    hungry-delete
+			    swiper
+			    counsel
+			    smartparens
+			    ;; theme
+			    monokai-theme
+			    solarized-theme
+			    ) "Default packages")
+(setq package-selected-packages linwenbo/packages)
+
+(defun linwenbo/packages-installed-p ()
+  (loop for pkg in linwenbo/packages
+	when (not (package-installed-p pkg)) do (return nil)
+	finally (return t)))
+(unless (linwenbo/packages-installed-p)
+  (message "%s" "refreshing pakcage database...")
+  (package-refresh-contents)
+  (dolist (pkg linwenbo/packages)
+    (when (not (package-installed-p pkg))
+    (package-install pkg))))
+
+
+(load-theme 'monokai t)
 
 ;; disable toolbar & scroll bar
 (tool-bar-mode -1)
@@ -35,7 +73,6 @@
 ;; fullscreen when start-up
 (setq initial-frame-alist (quote ((fullscreen . maximized))))
 
-(global-hl-line-mode t)
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 
 ;; use C-x C-r to open recent file
@@ -47,15 +84,3 @@
 
 
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
