9483f290b90646d210e2939a61024ac4ce55a248
linwenbo <linwenbo1994@gmail.com>
2020-01-08 13:30:50 +0800

    test

---
+++ ui-config.el
@@ -0,0 +1,15 @@
+
+;; 使用monokai主题
+(load-theme 'monokai t)
+
+;; 关闭工具栏和滚动条
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+;; 开启行号
+(global-linum-mode 1)
+
+;; 关闭欢迎页，开启后进入scratch buffer
+(setq inhibit-splash-screen t)
+
+
